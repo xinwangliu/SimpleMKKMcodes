0fbcd385273f95654f939940da059214fc78041c
LWXuan <378596974@qq.com>
2020-11-20 21:39:17 +0800

    1

--- SimpleMKKMdemo_Flower17.m
+++ SimpleMKKMdemo_Flower17.m
@@ -49,41 +49,7 @@ avgKer  = mycombFun(KH,Sigma1);
 %%%%---MKKM-----%%%
 [H_normalized2,Sigma2,obj2] = mkkmeans_train(KH,numclass,qnorm);
 [res_mean(:,2),res_std(:,2)] = myNMIACCV2(H_normalized2,Y,numclass);
-% 
-% % %%%%%%%%%%%%---LMKKM (NIPS-2014)---%%%%%%%%%%%%%%%%%%%%%%%
-% % [H_normalized3,obj3]= lmkkmeans_train(KH, numclass);
-% % [res_mean(:,3),res_std(:,3)]= myNMIACCV2(H_normalized3,Y,numclass);
-% 
-% % %%%%%%%%%%%%%----ONKC (AAAI 2017)----------%%%%%%%%%%%%%%%%%%%%%%%
-% rhoset4 = 2.^[0];
-% lambdaset4 = 2.^[0];
-% [H_normalized4,Sigma4,G4,obj4] = myoptimalNeighborhoodkernelclustering(KH,M,numclass,rhoset4,lambdaset4);
-% [res_mean(:,4),res_std(:,4)]= myNMIACCV2(H_normalized4,Y,numclass);
-% 
-% %%%%%%%%%---MKKM-MiR(AAAI-16)----%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% lambdaset5 = 2.^[-1];
-% [H_normalized5,Sigma5,obj5] = myregmultikernelclustering(KH,M,numclass,lambdaset5);
-% [res_mean(:,5),res_std(:,5)] = myNMIACCV2(H_normalized5,Y,numclass);
-% 
-% % % %%%%%---LKAM (IJCAI-2016)--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % lambdaset6 = 2.^[0];
-% % tauset6 = [0.5];
-% % numSel = round(tauset6*num);
-% % NS6 = genarateNeighborhood(avgKer,numSel);
-% % %%--Calculate Neighborhood--%%%%%%
-% % A6 = zeros(num);
-% % for i =1:num
-% %     A6(NS6(:,i),NS6(:,i)) = A6(NS6(:,i),NS6(:,i))+1;
-% % end
-% % HE6 = calHessian(KH,NS6,1);
-% % [H_normalized6,Sigma6,obj6] = mylocalizedregmultikernelclustering(KH,numclass,qnorm,HE6,A6,lambdaset6);
-% % [res_mean(:,6),res_std(:,6)]= myNMIACCV2(H_normalized6,Y,numclass);
-% 
-% %%%%%%%%%---(IJCAI-19)----%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% lambda7 = 2.^[1]; 
-% [H_normalized7,WP7,Sigma7,obj7] = myLateFusionMKC_lambda(KH,numclass,qnorm,lambda7);
-% [res_mean(:,7),res_std(:,7)] = myNMIACCV2(H_normalized7,Y,numclass);
 
 %%--- The Proposed SimpleMKKM----
 [H_normalized8,Sigma8,obj8] = MKKM_minmax(KH,numclass,options);
-[res_mean(:,8),res_std(:,8)] = myNMIACCV2(H_normalized8,Y,numclass);
+[res_mean(:,3),res_std(:,3)] = myNMIACCV2(H_normalized8,Y,numclass);
--- myNMIACCV2.m
+++ myNMIACCV2.m
@@ -1,4 +1,4 @@
-function [res_mean,res_std,res1,res2,res3,res4]= myNMIACCV2(U,Y,numclass)
+function [res_mean,res_std,res1,res2,res3]= myNMIACCV2(U,Y,numclass)
 
 stream = RandStream.getGlobalStream;
 reset(stream);
@@ -19,7 +19,6 @@ maxIter = 30;
 res1 = zeros(maxIter,1);
 res2 = zeros(maxIter,1);
 res3 = zeros(maxIter,1);
-res4 = zeros(maxIter,1);
 for it = 1 : maxIter
     indx = litekmeans(U_normalized,numclass, 'MaxIter',100, 'Replicates',10);
     %% indx = kmeans(U_normalized,numclass, 'MaxIter',100, 'Replicates',maxIter);
@@ -28,13 +27,12 @@ for it = 1 : maxIter
     res1(it) = mean(Y==newIndx);
     res2(it) = MutualInfo(Y,newIndx);
     res3(it) = purFuc(Y,newIndx);
-    res4(it) = adjrandindex(Y,newIndx);
+
 end
 res_mean(1) = mean(res1);
 res_mean(2) = mean(res2);
 res_mean(3) = mean(res3);
-res_mean(4) = mean(res4);
+
 res_std(1) = std(res1);
 res_std(2) = std(res2);
 res_std(3) = std(res3);
-res_std(4) = std(res4);
